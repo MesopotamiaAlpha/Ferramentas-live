f6e4da57443fd17bc355b7bbf4fed336f2020a33
MesopotamiaAlpha <leao-gomes@hotmail.com>
2021-03-04 16:34:17 -0300

    Atualizações

--- js/cadastro.js
+++ js/cadastro.js
@@ -2,9 +2,6 @@
 //na linha de baixo ele vai criar a coleção com o documento nome
 const docRef = firestore.doc("nomes/subs");
 
-//
-const outputHeader = document.querySelector("#nomeOutput");
-
 //aquele ele vai pegar o campo dentro do input do html para salvar 
 const inputTextField = document.querySelector("#inputNome");
 
@@ -16,14 +13,24 @@ const saveButton = document.querySelector("#saveButton");
 //função de salvar no banco de dados
 saveButton.addEventListener("click", function() {
     const textToSave = inputTextField.value;
-    console.log("Eu vou salvar " + textToSave + " no Firestore");
+    console.log("Eu vou salvar " + textToSave + " no Firestore " + docRef.id);
     docRef.set({
         
         //status nome vai ser o nome do campo para registrar no bd
         nickSub: textToSave
     }).then(function (){
         window.alert("Status salvado!");
+        recuperarNomes();
     }).catch(function  (error) {
         window.alert("aconteceu um erro: ", error);
     });
     })
+
+
+
+    function recuperarNomes(){
+        async getMarker() {
+            const snapshot = await firebase.firestore().collection('events').get()
+            return snapshot.docs.map(doc => doc.data());
+        }
+    }
