b5199276fc2472d9c56b8ffb9068836482d47193
MesopotamiaAlpha <leao-gomes@hotmail.com>
2021-04-05 15:59:08 -0300

    Adibionado atualização em tempo real fila subs

--- js/bd-fila.js
+++ js/bd-fila.js
@@ -1,13 +1,11 @@
-var nome = document.getElementById("Nome");
-var tipo = document.getElementById("Tipo");
-var marca = document.getElementById("Marca");
+var nick = document.getElementById("Nick");
+
 
 function writeData(){
     var Index = document.getElementById("Index").value
     firebase.database().ref("filaSubs/" + Index).set({
-        tipo: document.getElementById("Tipo").value,
-        nome: document.getElementById("Nome").value,
-        marca: document.getElementById("Marca").value
+        nick: document.getElementById("Nick").value
+
     });
     getData();
 }
@@ -18,7 +16,7 @@ function getData(){
         {
             var childKey = childSnapshot.key;
             var childData = childSnapshot.val();
-            document.getElementById("data").innerHTML = childData['tipo'] +", " + childData['nome']+ ", " + childData['marca'] ;
+            document.getElementById("data" + childKey).innerHTML = childData['nick'];
         })
     })
     
@@ -27,9 +25,7 @@ function getData(){
 function updateData(){
     var rootRef = firebase.database().ref("filaSubs/");
     const newData = {
-        nome: nome.value,
-        tipo: tipo.value,
-        marca: marca.value
+        nick: nick.value
     };
     rootRef.child(Index.value).update(newData);
     
@@ -42,18 +38,3 @@ function deleteData(){
     
 }
 
-
-firebase.collection("filaSubs/").where("state", "==", "CA")
-    .onSnapshot((snapshot) => {
-        snapshot.docChanges().forEach((change) => {
-            if (change.type === "added") {
-                console.log("New city: ", change.doc.data());
-            }
-            if (change.type === "modified") {
-                console.log("Modified city: ", change.doc.data());
-            }
-            if (change.type === "removed") {
-                console.log("Removed city: ", change.doc.data());
-            }
-        });
-    });
--- js/fila-subs-atualizar-nome.js
+++ js/fila-subs-atualizar-nome.js
@@ -1,19 +1,8 @@
 (function() {
-    const config = {
-        apiKey: "AIzaSyAcMZ3IFNoPMh503iSpIQhEzZDqg5ZiAvI",
-        authDomain: "projeto-live-ecaa3.firebaseapp.com",
-        databaseURL: "https://projeto-live-ecaa3-default-rtdb.firebaseio.com",
-        projectId: "projeto-live-ecaa3",
-        storageBucket: "projeto-live-ecaa3.appspot.com",
-        messagingSenderId: "481217991939",
-        appId: "1:481217991939:web:4460565326eedc9f83c536",
-        measurementId: "G-VJW8QMP718"
-    };
 
-    firebase.initializeApp(config);
     const bigTextEvaluationStudents = document.getElementById('bigTextEvaluationStudents');
     const dbBigTextEvaluationStudentsRef = firebase.database().ref().child('bigTextEvaluationStudents');
-    dbBigTextEvaluationStudentsRef.on('value', snap => bigTextEvaluationStudents.innerText = snap.val());
+   // dbBigTextEvaluationStudentsRef.on('value', snap => bigTextEvaluationStudents.innerText = snap.val());
 
     var table = document.querySelector('#table1 tbody');
     const dbEvaluationStudentsRef = firebase.database().ref().child('filaSubs/');
